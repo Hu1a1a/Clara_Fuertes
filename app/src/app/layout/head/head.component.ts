3e0eab4d084bb6c62e6b22195890e8112e00e61c
Hulala <yang.ye.1@hotmail.com>
2024-04-24 19:49:21 +0200

    Update diseño.

--- app/src/app/layout/head/head.component.ts
+++ app/src/app/layout/head/head.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-head',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
-export class HeadComponent { }
+export class HeadComponent {
+  Menu: any = [
+    { Name: "Inicio", Router: "#/" },
+    { Name: "Recursos gratuitos", Router: "#/" },
+    { Name: "Cursos para ti", Router: "#/" },
+    { Name: "Sana tus digestiones", Router: "#/" },
+    { Name: "Pérdida de grasa", Router: "#/" },
+    { Name: "Asesoramiento único", Router: "#/" },
+  ]
+}
