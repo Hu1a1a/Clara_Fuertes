0c583bc39cedce96a10f228f6da36896082daadd
Hulala <yang.ye.1@hotmail.com>
2024-04-28 21:38:49 +0200

    Upd

--- db/db.js
+++ db/db.js
@@ -1,12 +1,14 @@
+"use strict";
 const mysql = require("mysql");
+
 const pool = mysql.createPool({
-  connectionLimit: 4,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
+
 pool.getConnection((err, connection) => {
   if (err) throw err;
   console.log("Database connected successfully");
