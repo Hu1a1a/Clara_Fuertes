ba7181855fc710f01ef5a71afa811df533d8b636
Hulala <yang.ye.1@hotmail.com>
2024-05-13 22:38:23 +0200

    Update improve admin panel and footer.

--- app/src/app/app/admin/portal/app.component.ts
+++ app/src/app/app/admin/portal/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AngularMaterialModule } from '../../../module/app.angular.material.component';
 import { AngularModule } from '../../../module/app.angular.component copy';
 import { ApiService } from '../../../service/api.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-portal',
@@ -13,14 +12,23 @@ import { Router } from '@angular/router';
 })
 export class AppAdminPortalComponent implements OnInit {
   constructor(private api: ApiService) { }
-  panelOpenState = false;
-  level1!: any
-  level2!: any
-  video!: any
-  user!: any
-  comentario!: any
-  curso!: any
-  correo!: any
+  data: {
+    "curso/level1": any,
+    "curso/level2": any,
+    "curso/video": any,
+    "curso/curso": any,
+    "user": any,
+    "comentario": any,
+    "email": any,
+  } = {
+      "curso/level1": {},
+      "curso/level2": {},
+      "curso/video": {},
+      "curso/curso": {},
+      "user": {},
+      "comentario": {},
+      "email": {},
+    }
 
   ngOnInit() {
     this.Get()
@@ -28,45 +36,44 @@ export class AppAdminPortalComponent implements OnInit {
 
   modal: boolean = false
   sData!: any
-  sRouter!: string
+  sRouter!: "curso/level1" | "curso/level2" | "curso/video" | "curso/curso" | "user" | "comentario" | "email"
   sAccion!: "create" | "update" | "delete"
-
+  loading: boolean = true
   Key(data: any) {
     return Object.keys(data)
   }
 
   async Get() {
-    await this.api.Get("curso/level1").then((a) => this.level1 = a)
-    await this.api.Get("curso/level2").then((a) => this.level2 = a)
-    this.level2.data = this.level2.data.sort((a: any, b: any) => a.depId >= b.depId ? 1 : -1)
-    await this.api.Get("curso/video").then((a) => this.video = a)
-    this.video.data = this.video.data.sort((a: any, b: any) => a.Orden >= b.Orden ? 1 : -1)
-    await this.api.Get("user").then((a) => this.user = a)
-    await this.api.Get("comentario").then((a) => this.comentario = a)
-    await this.api.Get("curso/curso").then((a) => this.curso = a)
-    await this.api.Get("email").then((a) => this.correo = a)
+    this.data["curso/level1"] = await this.api.Get("curso/level1")
+    this.data["curso/level2"] = await this.api.Get("curso/level2")
+    this.data["curso/level2"].data = this.data["curso/level2"].data.sort((a: any, b: any) => a.depId >= b.depId ? 1 : -1)
+    this.data["curso/video"] = await this.api.Get("curso/video")
+    this.data["curso/video"].data = this.data["curso/video"].data.sort((a: any, b: any) => a.Orden >= b.Orden ? 1 : -1)
+    this.data["user"] = await this.api.Get("user")
+    this.data["comentario"] = await this.api.Get("comentario")
+    this.data["curso/curso"] = await this.api.Get("curso/curso")
+    this.data["email"] = await this.api.Get("email")
+    this.loading = false
   }
 
-  OpenModal(data: any, router: string, accion: "create" | "update" | "delete") {
+  OpenModal(data: any, router: "curso/level1" | "curso/level2" | "curso/video" | "curso/curso" | "user" | "comentario" | "email", accion: "create" | "update" | "delete") {
     this.sRouter = router
     this.sAccion = accion
     if (this.sAccion === "create") {
       this.sData = JSON.parse(JSON.stringify(data))
-      for (const d of Object.keys(this.sData)) {
-        this.sData[d] = ""
-      }
-    } else {
-      this.sData = data
-    }
+      for (const d of Object.keys(this.sData)) this.sData[d] = ""
+    } else this.sData = data
     this.modal = true
   }
 
+  Accion(data: any, router: "curso/level1" | "curso/level2" | "curso/video" | "curso/curso" | "user" | "comentario" | "email", accion: "create" | "update" | "delete") {
+    this.loading = true
+    setTimeout(async () => {
+      await this.api.Accion(data, router, accion)
+      this.data[router] = await this.api.Get(router)
+      this.modal = false
+      this.loading = false
+    }, 0);
 
-
-  async Accion(data: any, router: string, accion: "create" | "update" | "delete") {
-    await this.api.Accion(data, router, accion)
-    this.Get()
-    this.modal = false
   }
-
 }
