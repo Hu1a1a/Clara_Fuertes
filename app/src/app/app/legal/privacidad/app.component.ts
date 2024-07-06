a18897fa9c2388b70d9b23548b2786c43af7636d
Hulala <yang.ye.1@hotmail.com>
2024-07-06 23:34:01 +0200

    Update sitemap

--- app/src/app/app/legal/privacidad/app.component.ts
+++ app/src/app/app/legal/privacidad/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppLegalPrivacidadComponent {
-
-}
+export class AppLegalPrivacidadComponent { }
