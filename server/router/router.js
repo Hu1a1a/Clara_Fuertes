481c1e25bef9e3d87ce3d3094d7b1bf079786cbd
Hulala <yang.ye.1@hotmail.com>
2024-07-17 23:47:03 +0200

    Update perdida de gras

--- server/router/router.js
+++ server/router/router.js
@@ -47,7 +47,7 @@ router.post("/comentario/delete/", comentario.delete);
 //router.post("/stripe/pay/", stripe.paySession);
 //router.post("/stripe/check/", stripe.checkSession);
 router.post("/stripe/asesoramiento/pay/", stripe.asesoramiento);
-router.post("/stripe/webhook", stripe.webhook);
+router.post("/stripe/webhook/", stripe.webhook);
 
 router.get("/token/", auth.checkToken);
 router.post("/login/", auth.login);
